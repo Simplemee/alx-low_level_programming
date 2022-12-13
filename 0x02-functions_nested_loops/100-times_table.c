e02b93d2de2cc0e2296527618282cd324e43351b
Simplemee <mabuhaneef@gmail.com>
2022-12-13 08:45:39 -0800

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,6 +25,7 @@ void print_times_table(int n)
 					_putchar(' ');
 				if (prod <= 9)
 					_putchar(' ');
+
 				if (prod >= 100)
 				{
 					_putchar((prod / 100) + '0');
@@ -36,6 +37,7 @@ void print_times_table(int n)
 				}
 				_putchar((prod % 10) + '0');
 			}
+			_putchar('\n');
 		}
 	}
 }
