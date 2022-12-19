c0915c16222fa1a1a5341d2a512e8f90b31ab963
simplemee <mabuhaneef@gmail.com>
2022-12-19 06:32:35 -0800

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,10 +3,10 @@
 /**
  * reset_to_98 - takes the pointer and updates the value of n to 98
  * @n: input to check
- * Return: n
+ * Return: 0 is success 
  */
 
 void reset_to_98(int *n)
 {
-	*n = 98
+	*n = 98;
 }
