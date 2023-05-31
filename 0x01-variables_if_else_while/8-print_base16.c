104c4df55e22e5890d2cd19ebff0c304f3e8513e
simplemee <mabuhaneef@gmail.com>
2023-05-31 07:25:22 -0700

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,7 +12,7 @@ int main(void)
 	int n;
 	char lower_case;
 
-	for (n = 48; n <= 47; n++)
+	for (n = 48; n <= 57; n++)
 		putchar (n);
 	for (lower_case = 'a'; lower_case <= 'f'; lower_case++)
 		putchar (lower_case);
