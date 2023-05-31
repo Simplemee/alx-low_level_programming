fa7742f9b1b20ea0c7ce3d4174d18205f5bd9e06
simplemee <mabuhaneef@gmail.com>
2023-05-31 02:47:38 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	char Lowercase_letters;
+
+	for (Lowercase_letters + 'a'; Lowercase_letters <= 'z'; ++Lowercase_letters)
+		putchar(Lowercase_letters);
+	putchar('\n');
+
+	return (0);
+}
