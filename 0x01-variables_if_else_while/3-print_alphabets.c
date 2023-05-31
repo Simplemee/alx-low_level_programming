a5a621270b004f6f3570d26b34ba08849ba56b6e
simplemee <mabuhaneef@gmail.com>
2023-05-31 06:16:02 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	char alphabets;
+
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+		putchar(alphabets);
+
+	for (alphabets = 'A'; alphabets <= 'Z'; alphabets++)
+		putchar(alphabets);
+
+	putchar('\n');
+
+	return (0);
+}
