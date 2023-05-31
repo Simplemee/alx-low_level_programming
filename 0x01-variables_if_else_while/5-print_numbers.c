8dfefc591bc688b2b78e05b850f028ef655fa992
simplemee <mabuhaneef@gmail.com>
2023-05-31 06:39:00 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	int y;
+
+	for (y = 0; y < 10; y++)
+	{
+		printf("%d", y);
+	}
+	printf("\n");
+
+	return (0);
+}
